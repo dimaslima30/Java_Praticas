5f4d28bad42903c15f155307ba44f343bf73ade8
dimaslima30 <dimaslima1299@gmail.com>
2022-08-30 17:10:31 -0300

    What is the day of the week?

---
+++ Exercicios/exercicios_Iniciante/daysOfTheWeek.java
@@ -0,0 +1,42 @@
+package exercicios_Iniciante;
+
+import java.util.Scanner;
+
+public class daysOfTheWeek {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		String day;
+		switch (x) {
+		case 1:
+			day = "Sunday";
+			break;
+		case 2:
+			day = "Monday";
+			break;
+		case 3:
+			day = "Tuesday";
+			break;
+		case 4:
+			day = "Wednesday";
+			break;
+		case 5:
+			day = "Thursday";
+			break;
+		case 6:
+			day = "Friday";
+			break;
+		case 7:
+			day = "Saturday";
+			break;
+		default:
+			day = "Invalid value";
+			break;
+		}
+		System.out.println("Day of the week: " + day);
+		sc.close();
+
+	}
+
+}
